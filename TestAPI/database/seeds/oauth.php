2abd15dcbc87d25e5c686a9d31340e716bf16063
dvhieu18it4 <44391096+dvhieu18it4@users.noreply.github.com>
2020-08-01 02:43:21 +0700

    Update oauth.php

--- TestAPI/database/seeds/oauth.php
+++ TestAPI/database/seeds/oauth.php
@@ -12,8 +12,8 @@ class oauth extends Seeder
     public function run()
     {
         DB::table('oauth_clients')->insert([
-    	['id' => '1', 'name' => 'Laravel Personal Access Client','secret'=>'ql2kXVrfAmnQGinSr9NdsD6qX5IEUQr5gOqNv4db','redirect' => 'http://xecaonguyen.herokuapp.com','personal_access_client'=>'1','password_client' => '0','revoked'=>'0'],
-    	['id' => '2', 'name' => 'Laravel Password Grant Client','secret'=>'0xsb79VttZl5iUvgqeIgg6hmi1dtscscRq1zgdrU','provider' => 'user','redirect' => 'http://xecaonguyen.herokuapp.com','personal_access_client'=>'0','password_client' => '1','revoked'=>'0'],
+    	['id' => 1, 'name' => 'Laravel Personal Access Client','secret'=>'ql2kXVrfAmnQGinSr9NdsD6qX5IEUQr5gOqNv4db','redirect' => 'http://xecaonguyen.herokuapp.com','personal_access_client'=>1,'password_client' => 0,'revoked'=>0],
+    	['id' => 2, 'name' => 'Laravel Password Grant Client','secret'=>'0xsb79VttZl5iUvgqeIgg6hmi1dtscscRq1zgdrU','provider' => 'user','redirect' => 'http://xecaonguyen.herokuapp.com','personal_access_client'=>0,'password_client' => 1,'revoked'=>0],
 		]);
     }
 }
